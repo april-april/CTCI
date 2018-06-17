9a736e2031f0c2b798c170b1764cba758b824e17
Vincent L <vincentluoster93@gmail.com>
2018-06-16 21:22:40 -0700

    remove dupes

---
+++ LinkedList/1-remove_dupes.py
@@ -0,0 +1,17 @@
+from LinkedList import LinkedList
+
+
+def remove_dups(ll):
+    if ll.head is None:
+        return
+
+    current = ll.head
+    seen = set([current.value])
+    while current.next:
+        if current.next.value in seen:
+            current.next = current.next.next
+        else:
+            seen.add(current.next.value)
+            current = current.next
+
+    return ll
