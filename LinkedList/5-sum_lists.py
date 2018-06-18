7c71b5af5a4ec97ce749b76fc1a74a6feb1ebbe4
Vincent L <vincentluoster93@gmail.com>
2018-06-18 01:59:26 -0700

    update

---
+++ LinkedList/5-sum_lists.py
@@ -0,0 +1,25 @@
+#credit: CTCI
+
+from LinkedList import LinkedList
+
+
+def sum_lists(ll_a, ll_b):
+    n1, n2 = ll_a.head, ll_b.head
+    ll = LinkedList()
+    carry = 0
+    while n1 or n2:
+        result = carry
+        if n1:
+            result += n1.value
+            n1 = n1.next
+        if n2:
+            result += n2.value
+            n2 = n2.next
+
+        ll.add(result % 10)
+        carry = result // 10
+
+    if carry:
+        ll.add(carry)
+
+    return ll
