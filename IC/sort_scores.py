105742d8c0973c102f62135c4fd57dbcb4925037
Vincent L <vincentluoster93@gmail.com>
2018-06-30 22:35:49 -0700

    update

--- IC/sort_scores.py
+++ IC/sort_scores.py
@@ -1,3 +1,14 @@
 def sort_scores(unsorted_scores, highest_possible_score):
     # List of 0s at indices 0..highest_possible_score
     score_counts = [0] * (highest_possible_score+1)
+
+    # Populate score_counts
+    for score in unsorted_scores:
+        score_counts[score] += 1
+
+    # Populate the final sorted list
+    sorted_scores = []
+
+    # For each item in score_counts
+    for score in xrange(len(score_counts) - 1, -1, -1):
+        count = score_counts[score]
