646bfbc13c6b79a781cc5b14e54d3a5071bd3c55
Vincent L <vincentluoster93@gmail.com>
2018-06-30 21:37:03 -0700

    changes

--- IC/find_rotation_point.py
+++ IC/find_rotation_point.py
@@ -6,3 +6,11 @@ def find_rotation_point(words):
     while floor_index < ceiling_index:
         # Guess a point halfway between floor and ceiling
         guess_index = floor_index + ((ceiling_index - floor_index) / 2)
+
+        # If guess comes after first word or is the first word
+        if words[guess_index] >= first_word:
+            # Go right
+            floor_index = guess_index
+        else:
+            # Go left
+            ceiling_index = guess_index
