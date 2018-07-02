d860ec47123c8801e6a900eddb4d84ae7f8a6cef
Vincent L <vincentluoster93@gmail.com>
2018-07-01 20:02:48 -0700

    update highest product

--- IC/Greedy/highest_product_3.py
+++ IC/Greedy/highest_product_3.py
@@ -2,5 +2,20 @@ def highest_product_of_3(list_of_ints):
     if len(list_of_ints) < 3:
         raise ValueError('Less than 3 items!')
 
+    # We're going to start at the 3rd item (at index 2)
+    # so pre-populate highests and lowests based on the first 2 items.
+    # We could also start these as None and check below if they're set
+    # but this is arguably cleaner
+    highest = max(list_of_ints[0], list_of_ints[1])
+    lowest  = min(list_of_ints[0], list_of_ints[1])
+    highest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+    lowest_product_of_2  = list_of_ints[0] * list_of_ints[1]
 
+    # Except this one--we pre-populate it for the first *3* items.
+    # This means in our first pass it'll check against itself, which is fine.
+    highest_product_of_3 = list_of_ints[0] * list_of_ints[1] * list_of_ints[2]
+
+    # Walk through items, starting at index 2
     #TO BE CONTINUED
+
+    return highest_product_of_3
