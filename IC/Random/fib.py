37014997d2a744613284de44d4b9729936b35af2
Vincent L <vincentluoster93@gmail.com>
2018-07-03 17:46:59 -0700

    fib

---
+++ IC/Random/fib.py
@@ -0,0 +1,12 @@
+def fib(n):
+    if n < 0:
+        raise IndexError(
+            'Index was negative. '
+            'No such thing as a negative index in a series.'
+        )
+    elif n in [0, 1]:
+        # Base cases
+        return n
+
+    print "computing fib(%i)" % n
+    return fib(n - 1) + fib(n - 2)
