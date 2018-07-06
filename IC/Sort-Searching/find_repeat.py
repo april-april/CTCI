12d1b8127b20310d95333be62a22f0b5ce321ce9
Vincent L <vincentluoster93@gmail.com>
2018-07-05 19:59:55 -0700

    update

--- IC/Sort-Searching/find_repeat.py
+++ IC/Sort-Searching/find_repeat.py
@@ -7,10 +7,24 @@ def find_repeat(numbers):
         else:
             numbers_seen.add(number)
 
-    # Whoops--no duplicate
+    # no duplicate
     raise Exception('no duplicate!')
 
-#Optimize for space to be O(1)
+#time O(n^2), space O(1)
+def find_repeat_brute_force(numbers):
+    for needle in range(1, len(numbers)):
+        has_been_seen = False
+        for number in numbers:
+            if number == needle:
+                if has_been_seen:
+                    return number
+                else:
+                    has_been_seen = True
+
+    # no duplicate
+    raise Exception('no duplicate!')
+
+#Optimize for better space and time
 
 
 
