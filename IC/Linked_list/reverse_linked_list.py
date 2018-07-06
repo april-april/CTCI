b4ea6cd447da59b8034fdd008e9c1982308b0f54
Vincent L <vincentluoster93@gmail.com>
2018-07-05 20:13:23 -0700

    reverse_linked-list

---
+++ IC/Linked_list/reverse_linked_list.py
@@ -0,0 +1,11 @@
+'''
+sample linked list node class:
+
+  class LinkedListNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+'''
+def reverse(head_of_list):
