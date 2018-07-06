6633cf6d306c62e15d58ab2753f0f1abbab50079
Vincent L <vincentluoster93@gmail.com>
2018-07-05 20:11:31 -0700

    delete ll

---
+++ IC/Linked_list/delete_node_ll.py
@@ -0,0 +1,16 @@
+#Be careful with this solution when deleting the last node in a ll
+#O(1) time and O(1) space.
+
+def delete_node(node_to_delete):
+    # Get the input node's next node, the one we want to skip to
+    next_node = node_to_delete.next
+
+    if next_node:
+        # Replace the input node's value and pointer with the next
+        # node's value and pointer. The previous node now effectively
+        # skips over the input node
+        node_to_delete.value = next_node.value
+        node_to_delete.next  = next_node.next
+    else:
+        # Eep, we're trying to delete the last node!
+        raise Exception("Can't delete the last node with this technique!")
