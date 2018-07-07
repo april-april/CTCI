457d2215e5d5cd05182628d6ee2dadb61d3f31bb
Vincent L <vincentluoster93@gmail.com>
2018-07-06 18:44:58 -0700

    bottom up approach

--- IC/Random/multiple_numbers_easy.py
+++ IC/Random/multiple_numbers_easy.py
@@ -1,5 +1,16 @@
 #top-down approach to multiple numbers in a range, recursion
-
+#vulnerable to stack-overflow
 def product_1_to_n(n):
     # We assume n >= 1
     return n * product_1_to_n(n - 1) if n > 1 else 1
+
+
+#bottom-up approach
+def product_1_to_n(n):
+    # We assume n >= 1
+    result = 1
+    for num in range(1, n + 1):
+        result *= num
+
+    return result
+
