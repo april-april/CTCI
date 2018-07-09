c54f5f14650bddcbb8105f308992b03bb1e20ebb
Vincent L <vincentluoster93@gmail.com>
2018-07-08 22:07:09 -0700

    example #2

--- JS/closures.js
+++ JS/closures.js
@@ -1,5 +1,18 @@
+//example of closure
 var message = 'The British are coming.';
 function sayMessage(){
     alert(message);  // here we have access to message,
     // even though it's declared outside this function!
 };
+
+//example #2
+var getUniqueId = (function(){
+    var nextGeneratedId = 0;
+    return function(element) {
+        if (!element.id) {
+            element.id = 'generated-uid-' + nextGeneratedId;
+            nextGeneratedId++;
+        }
+        return element.id;
+    };
+})();
