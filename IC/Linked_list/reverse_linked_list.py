e37b6cabf869a7bb4c70d89979a0b3ec52e2011a
Vincent L <vincentluoster93@gmail.com>
2018-07-08 22:15:11 -0700

    reverse-ll

--- IC/Linked_list/reverse_linked_list.py
+++ IC/Linked_list/reverse_linked_list.py
@@ -8,4 +8,24 @@ sample linked list node class:
         self.next  = None
 
 '''
+
 def reverse(head_of_list):
+  
+    current_node = head_of_list
+    previous_node = None
+    next_node = None
+
+    # Until we have 'fallen off' the end of the list
+    while current_node:
+        # Copy a pointer to the next element
+        # before we overwrite current_node.next
+        next_node = current_node.next
+
+        # Reverse the 'next' pointer
+        current_node.next = previous_node
+
+        # Step forward in the list
+        previous_node = current_node
+        current_node = next_node
+
+    return previous_node
