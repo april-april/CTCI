cb7d25fc9a24b964a998426a5e001c46545d2548
Vincent L <vincentluoster93@gmail.com>
2018-07-09 22:59:34 -0700

    xor

---
+++ IC/bit-manipulation/bitwise_XOR.py
@@ -0,0 +1,14 @@
+#Exclusive OR
+#returns 1 if exactly one of the bits is 1
+
+print 1 ^ 1  
+print 1 ^ 0  
+print 0 ^ 1  
+print 0 ^ 0  
+
+print 5 ^ 6  # gives 3
+
+# At the bit level:
+#     0101  (5)
+#   ^ 0110  (6)
+#   = 0011  (3)
