75d294e05ff38ef8b0c1e9401b7f3e3a2f92c3b4
Vincent L <vincentluoster93@gmail.com>
2018-07-10 22:06:30 -0700

    bit

---
+++ IC/bit-manipulation/bit_shifting.py
@@ -0,0 +1,9 @@
+#  0010 << 1  →  0100
+#  1011 >> 1  →  0101
+# #A single left shift multiplies a binary number by 2
+print 0b0010 
+print 0b0010 << 1
+
+#And similarly, shifting right divides by 2
+print 0b0100
+print 0b0100 >> 1
