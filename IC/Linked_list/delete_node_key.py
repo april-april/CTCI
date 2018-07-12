1268c4a2f9207f8d0fc285a5974f82cd73d6ba6b
Vincent L <vincentluoster93@gmail.com>
2018-07-11 23:03:52 -0700

    update

--- IC/Linked_list/delete_node_key.py
+++ IC/Linked_list/delete_node_key.py
@@ -1,6 +1,20 @@
 #Given head of a linked list and a key, 
 #delete node with this given key from the linked list.
-
 def delete_node(head, key):
-  prev = None
-  current = head
+    prev = None
+    current = head
+
+    while (current != None):
+        if current.data == key:
+            if current == head:
+                head = head.next
+                current = head
+            else:
+                prev.next = current.next
+                current = current.next
+      
+    else:
+        prev = current
+        current = current.next
+
+    return head
