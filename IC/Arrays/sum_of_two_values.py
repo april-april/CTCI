b189463054ee2a40e1b732fe5f3d3b960de0bce3
Vincent L <vincentluoster93@gmail.com>
2018-07-11 23:20:17 -0700

    tests for 2 sum

--- IC/Arrays/sum_of_two_values.py
+++ IC/Arrays/sum_of_two_values.py
@@ -12,3 +12,19 @@ def find_sum_of_two(array, val):
     
     return False
 
+def test(v, val, expected):
+    output = find_sum_of_two(v, val)
+    print("exist(A, " + str(val) + ") = " + str(output) + "\n")
+    assert expected == output
+
+def main():
+    v = [2, 1, 8, 4, 7, 3]
+    test(v, 3, True)
+    test(v, 20, False)
+    test(v, 1, False)
+    test(v, 2, False)
+    test(v, 7, True)
+
+main()
+
+
