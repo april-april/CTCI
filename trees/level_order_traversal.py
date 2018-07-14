576da8e8f619a077be50ed98f4f619a6ecebd373
Vincent L <vincentluoster93@gmail.com>
2018-07-13 22:37:32 -0700

    lot

--- trees/level_order_traversal.py
+++ trees/level_order_traversal.py
@@ -9,9 +9,31 @@ def level_order_traversal_1(root):
     if root == None:
         return
 
+     
+
     queues = [collections.deque(), collections.deque()]
 
     current_queue = queues[0]
     next_queue = queues[1]
 
     current_queue.append(root)
+    level_number = 0
+
+    while current_queue:
+        temp = current_queue.popleft()
+        print str(temp.data) + " ",
+
+        if temp.left != None:
+            next_queue.append(temp.left)
+
+        if temp.right != None:
+            next_queue.append(temp.left)
+
+        if not current_queue:
+            print
+            level_number += 1
+            current_queue = queues[level_number % 2]
+            next_queue = queues[(level_number + 1) % 2]
+    
+    print
+
