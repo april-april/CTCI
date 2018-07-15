20a2b11db377ff29459c8a36aa1d027de7bc7b76
Vincent L <vincentluoster93@gmail.com>
2018-07-14 21:54:49 -0700

    dequeue update

--- stacks-queues/queue_using_stacks.py
+++ stacks-queues/queue_using_stacks.py
@@ -12,3 +12,15 @@ class queue_using_stack:
         return len(self.stack1) == 0 and len(self.stack2) == 0
     
     def dequeue(self):
+        if self.empty():
+            raise Empty
+
+        if len(self.stack2) == 0:
+            while len(self.stack1) != 0:
+                val = self.stack1[-1]
+                self.stack1.pop()
+                self.stack2.append(val)
+
+        val = self.stack2[-1]
+        self.stack2.pop()
+        return val
