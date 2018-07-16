515a6b14a338d1283c263da9012d0ce0575a571d
Vincent L <vincentluoster93@gmail.com>
2018-07-15 22:44:03 -0700

    update

---
+++ LinkedList/swap_nth_node_with_head.py
@@ -0,0 +1,28 @@
+# Node indices starts from 1.
+def swap_nth_node(head, n):
+    prev = None
+    current = head
+
+    if head == None:
+        return head
+
+    if n == 1:
+        # No need to swap head with head.
+        return head
+    
+    count = 1
+
+    while current != None and count < n:
+        prev = current
+        current = current.next
+        count += 1
+
+    if current == None:
+        return head
+    
+    # current is pointing to nth node
+
+    prev.next = head
+    temp = head.next
+    head.next = current.next
+    current.next = temp
