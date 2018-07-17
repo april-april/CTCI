45a26de0fe8b9ac82d4a5d450ecd9120ce00ef0f
Vincent L <vincentluoster93@gmail.com>
2018-07-16 23:39:15 -0700

    update

--- trees/in_order_traversal.py
+++ trees/in_order_traversal.py
@@ -1,3 +1,10 @@
+import collections
+
 def inorder_iterative(root):
     if root == None:
         return
+
+    stack = collections.deque()
+    count = 0
+
+    while (len(stk) != 0 or root != None): 
