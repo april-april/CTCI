ebd009c79d3b37908e3e2c61ff50b482f0623488
Vincent L <vincentluoster93@gmail.com>
2018-07-17 23:57:51 -0700

    merge_sort-final

--- Sort/merge_sort.py
+++ Sort/merge_sort.py
@@ -1,3 +1,5 @@
+#implementation of mergeSort from algorithms book
+
 def mergeSort(alist):
 
     print("Splitting ", alist)
@@ -8,3 +10,35 @@ def mergeSort(alist):
 
         mergeSort(lefthalf)
         mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+        
+    print("Merging ",alist)
+
+def main():
+    alist = [54,26,93,17,77,31,44,55,20]
+    mergeSort(alist)
+    print(alist)
+
+main()
