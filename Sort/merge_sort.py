4f6a206736260141280f33d5465af6698ac4a1f5
Vincent L <vincentluoster93@gmail.com>
2018-07-17 23:53:14 -0700

    merge_sort_start

---
+++ Sort/merge_sort.py
@@ -0,0 +1,10 @@
+def mergeSort(alist):
+
+    print("Splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
