39e92720485b275ea8e0e5d992130a1ed1bda1aa
Vincent L <vincentluoster93@gmail.com>
2018-07-23 23:05:54 -0700

    bfs fix

--- graphs/breadth-first-search.py
+++ graphs/breadth-first-search.py
@@ -1,25 +1,24 @@
-#efficient solution
+#breadth first search
 import collections
 
 def breadth_first_search(graph, root):
 
-    visited, queue = set(), collections.deque([root])
+    seen, queue = set(), collections.deque([root])
 
     while queue: 
 
         vertex = queue.popleft()
         
         for neighbour in graph[vertex]:
-
-            if neighbour not in visited: 
-                
-                visited.add(neighbour) 
+            if neighbour not in seen: 
+                seen.add(neighbour) 
                 queue.append(neighbour)
-                print(visited) 
+                print(seen)
+    return seen 
 
 
 if __name__ == '__main__':
-    graph = {0: [1, 2], 1: [2], 2: []} 
+    graph = {0: [1, 2, 3], 1: [2], 2: [], 3: []} 
     breadth_first_search(graph, 0)
 
 
