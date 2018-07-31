9fc343e03f55f510f83f22a189da98da74110f76
Vincent L <vincentluoster93@gmail.com>
2018-07-30 23:08:22 -0700

    tests

--- Arrays/move_zeros_to_left.py
+++ Arrays/move_zeros_to_left.py
@@ -1,22 +1,28 @@
 # move all elements containing '0' to the left
 # while maintaining the order of other elements in the array
 
-def move_zeros_to_left(A):
-    if len(A) < 1:
+def move_zeros_to_left(arr):
+    if len(arr) < 1:
         return
 
-    length = len(A)
+    length = len(arr)
     write_index = length - 1
     read_index = length - 1
 
     while(read_index >= 0):
-        if A[read_index] != 0:
-            A[write_index] = A[read_index]
+        if arr[read_index] != 0:
+            arr[write_index] = arr[read_index]
             write_index -= 1
 
         read_index -= 1
 
     while(write_index >= 0):
-        A[write_index] = 0
+        arr[write_index] = 0
         write_index -= 1
 
+def main():
+    test = [1, 10, -1, 11, 5, 0, -7, 0, 25, -35]
+    move_zeros_to_left(test)
+    print(test)
+
+main()
