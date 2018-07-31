15d4fa40af7421ddb0a460c74b7fdaffc131f4ba
Vincent L <vincentluoster93@gmail.com>
2018-07-31 00:41:11 -0700

    start

---
+++ Arrays/find_smallest_common_number.py
@@ -0,0 +1,10 @@
+# Given three integer arrays sorted in ascending order, 
+# find the smallest number that is common in all three arrays.
+def find_smallest_common_number(a,b,c):
+
+    #initialize 3 pointers
+    i = 0
+    j = 0
+    k = 0
+
+    return
