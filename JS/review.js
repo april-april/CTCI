e7371c827199271a4fff3b5940e5f9524dfc6174
Vincent L <vincentluoster93@gmail.com>
2018-08-02 16:04:08 -0700

    closures review

---
+++ JS/review.js
@@ -0,0 +1,15 @@
+function baz() {
+    var x = 1;
+    return {
+        foo: function foo() { return ++x; },
+        bar: function bar() { return --x; }
+  };
+}
+ 
+var closures = baz();
+ 
+console.log(
+    closures.foo(), // 2
+    closures.bar(), // 1
+    closures.foo()  //2
+);
