2fee9a4cc817e8d3db8560fcdf49ee660a82fdcb
Vincent L <vincentluoster93@gmail.com>
2018-08-02 20:00:44 -0700

    hero proto js

---
+++ JS/js_concepts_review/prototype_and_inheritance.js
@@ -0,0 +1,17 @@
+// Initialize constructor functions
+function Hero(name, level) {
+  this.name = name;
+  this.level = level;
+}
+
+function Warrior(name, level, weapon) {
+  Hero.call(this, name, level);
+
+  this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+  Hero.call(this, name, level);
+
+  this.spell = spell;
+}
