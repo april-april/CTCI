f8da6b6dde62cb17b68ec77f80da18c0af57fa80
Vincent L <vincentluoster93@gmail.com>
2018-08-02 20:03:31 -0700

    hero

--- JS/js_concepts_review/prototype_and_inheritance.js
+++ JS/js_concepts_review/prototype_and_inheritance.js
@@ -15,3 +15,23 @@ function Healer(name, level, spell) {
 
   this.spell = spell;
 }
+
+// Link prototypes and add prototype methods
+Warrior.prototype = Object.create(Hero.prototype);
+Healer.prototype = Object.create(Hero.prototype);
+
+Hero.prototype.greet = function () {
+  return `${this.name} says hello.`;
+}
+
+Warrior.prototype.attack = function () {
+  return `${this.name} attacks with the ${this.weapon}.`;
+}
+
+Healer.prototype.heal = function () {
+  return `${this.name} casts ${this.spell}.`;
+}
+
+// Initialize individual character instances
+const hero1 = new Warrior('Bjorn', 1, 'axe');
+const hero2 = new Healer('Kanin', 1, 'cure');
